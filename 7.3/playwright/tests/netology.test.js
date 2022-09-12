613dbe11448ee05cc8ec0a9680d6533c27cee823
Elizar Abramov <nezha2077@mail.ru>
2022-09-12 18:27:24 +0700

    tests were modified

--- 7.3/playwright/tests/netology.test.js
+++ 7.3/playwright/tests/netology.test.js
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test';
+const { mail, password } = require('./user.js');
 
 test('successfulAuthorization', async ({ page }) => {
     test.setTimeout(120000);
@@ -7,11 +8,12 @@ test('successfulAuthorization', async ({ page }) => {
     // Click [placeholder="Email"]
     await page.locator('[placeholder="Email"]').click();
     // Fill [placeholder="Email"]
-    await page.locator('[placeholder="Email"]').fill('nezha2077@mail.ru');
+    await page.locator('[placeholder="Email"]').fill(mail);
     // Click [placeholder="Пароль"]
+    debugger;
     await page.locator('[placeholder="Пароль"]').click();
     // Fill [placeholder="Пароль"]
-    await page.locator('[placeholder="Пароль"]').fill('03021970oooOOO');
+    await page.locator('[placeholder="Пароль"]').fill(password);
     // Click [data-testid="login-submit-btn"]
     await page.locator('[data-testid="login-submit-btn"]').click();
     await expect(page).toHaveURL('https://netology.ru/profile');
