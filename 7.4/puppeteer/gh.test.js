15dc6335f0a40aa2e62901ca4748b33eeffdac2e
Elizar Abramov <nezha2077@mail.ru>
2022-09-14 16:04:01 +0700

    tests modified

--- 7.4/puppeteer/gh.test.js
+++ 7.4/puppeteer/gh.test.js
@@ -2,7 +2,6 @@ let page;
 
 beforeEach(async () => {
   page = await browser.newPage();
-  await page.goto("https://github.com/team");
 });
 
 afterEach(() => {
@@ -10,16 +9,21 @@ afterEach(() => {
 });
 
 describe("Github page tests", () => {
+
+  beforeEach(async () => {
+    await page.goto("https://github.com/team");
+  })
+
   test("The h1 header content'", async () => {
     const firstLink = await page.$("header div div a");
     await firstLink.click();
     await page.waitForSelector('h1');
     const title2 = await page.title();
-    expect(title2).toEqual('GitHub: Where the world builds software · GitHub');
+    expect(title2).toEqual('GitHub for teams · Build like the best teams on the planet · GitHub');
   });
 
   test("The first link attribute", async () => {
-    const actual = await page.$eval("a", link => link.getAttribute('href') );
+    const actual = await page.$eval("a", link => link.getAttribute('href'));
     expect(actual).toEqual("#start-of-content");
   });
 
@@ -30,5 +34,32 @@ describe("Github page tests", () => {
     });
     const actual = await page.$eval(btnSelector, link => link.textContent);
     expect(actual).toContain("Sign up for free")
+  });
+});
+
+describe("Github page tests", () => {
+
+  test("The h1 header content on about page", async () => {
+    await page.goto("https://github.com/about");
+    await page.waitForSelector('h1');
+    const title2 = await page.title();
+    expect(title2).toEqual("About · GitHub");
+
+  });
+
+  test("The h1 header content on features page", async () => {
+    await page.goto("https://github.com/features");
+    await page.waitForSelector('h1');
+    const title2 = await page.title();
+    expect(title2).toEqual("Features | GitHub · GitHub");
+
+  });
+
+  test("The h1 header content on privacy page", async () => {
+    await page.goto("https://github.com/privacy");
+    await page.waitForSelector('h1');
+    const actual = await page.title();
+    expect(actual).toEqual("privacy · GitHub");
+
   });
 });
